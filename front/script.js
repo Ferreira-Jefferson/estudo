9991006476b05f9f8c9dcd8a21359d240d896a0b
Jefferson Ferreira <jefferson.ctc.ferreira@outlook.com>
2024-12-15 21:59:32 +0000

    front

---
+++ front/script.js
@@ -0,0 +1,11 @@
+window.onload = async function() {
+  const response = await fetch('http://degustware.com.br/api');
+  const products = await response.json();
+
+  const productList = document.getElementById('product-list');
+  products.forEach(product => {
+    const li = document.createElement('li');
+    li.textContent = `${product.name} - R$${product.price}`;
+    productList.appendChild(li);
+  });
+};
