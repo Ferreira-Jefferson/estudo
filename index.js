cef46fc60c86cfbcee74a66a9a16ae531cdc3fe6
Jefferson Ferreira <jefferson.ctc.ferreira@outlook.com>
2024-12-15 13:54:56 +0000

    fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 80; // Porta padrão para HTTP
 
 app.get('/', (req, res) => {
-    res.send('<h1>Bem-vindo ao Degustware!</h1><p>Servidor funcionando!</p>');
+    res.send('<h1>Bem-vindo ao Degustware!</h1><p>Tudo ok por aqui!</p>');
 });
 
 app.listen(port, () => {
