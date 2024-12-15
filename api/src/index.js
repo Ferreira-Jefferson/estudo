125396078d79d8d9ba155f70b082df264b427621
Jefferson Ferreira <jefferson.ctc.ferreira@outlook.com>
2024-12-15 17:14:29 +0000

    ip

--- api/src/index.js
+++ api/src/index.js
@@ -24,6 +24,6 @@ app.get('/products', function(req, res) {
 });
 
 
-app.listen(9001, 'http://degustware.com.br/', function() {
+app.listen(9001, '194.238.25.229', function() {
   console.log('Listening on port 9001');
 })
