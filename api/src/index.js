7bf630ac37f3039714320053e3373db2e539896e
Jefferson Ferreira <jefferson.ctc.ferreira@outlook.com>
2024-12-15 19:12:24 +0000

    fix

--- api/src/index.js
+++ api/src/index.js
@@ -14,15 +14,14 @@ connection.connect();
 
 app.get('/products', function(req, res) {
   connection.query('SELECT * FROM products', function (error, results) {
-    if (error) { 
+    if (error) {
       throw error;
-    };
+    }
     res.send(results.map(item => ({ name: item.name, price: item.price })));
   });
 });
 
-// Corrigindo o método app.listen()
 const port = 9001;
-app.listen(port, function() {
+app.listen(port, '0.0.0.0', function() {
   console.log(`Server is listening on port ${port}`);
 });
