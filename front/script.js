3f57d06f94429b81ce0a11ce719462d952220401
Jefferson Ferreira <jefferson.ctc.ferreira@outlook.com>
2024-12-18 23:18:30 +0000

    fix url

--- front/script.js
+++ front/script.js
@@ -1,11 +1,11 @@
-window.onload = async function () {
-  const response = await fetch("degustware.com.br/api")
-  const products = await response.json()
+window.onload = async function() {
+  const response = await fetch('https://degustware.com.br/api');
+  const products = await response.json();
 
-  const productList = document.getElementById("product-list")
+  const productList = document.getElementById('product-list');
   products.forEach(product => {
-    const li = document.createElement("li")
-    li.textContent = `${product.name} - R$${product.price}`
-    productList.appendChild(li)
-  })
-}
+    const li = document.createElement('li');
+    li.textContent = `${product.name} - R$${product.price}`;
+    productList.appendChild(li);
+  });
+};
