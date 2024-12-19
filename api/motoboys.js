34b46d6c03dea0bcb094140341be26814c82de20
Jefferson Ferreira <jefferson.ctc.ferreira@outlook.com>
2024-12-19 22:11:40 +0000

    fix

--- api/motoboys.js
+++ api/motoboys.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const mysql = require('mysql2');
 const mysql = require('mysql2/promise');
 const cors = require('cors');
 const app = express();
