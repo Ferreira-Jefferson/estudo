0553542ceaccbf4e0d3e14ce538275003235ea6f
Jefferson Ferreira <jefferson.ctc.ferreira@outlook.com>
2025-01-31 21:26:36 +0000

    fix

--- front/src/core/dbInfo.js
+++ front/src/core/dbInfo.js
@@ -1,10 +1,10 @@
 import path from "path";
 
-const containerName = "infra-db-1";
+const containerName = "mysql_db";
 
 export default  {
   backupFile: path.join("backups", "backup.sql"),
   containerName,
   databaseName: "syscon_bd",
-  baseCommand: `docker exec -e MYSQL_PWD=local_password -i ${containerName} mysql -uroot`,
+  baseCommand: `docker exec -e MYSQL_PWD=rootpassword -i ${containerName} mysql -uroot`,
 };
