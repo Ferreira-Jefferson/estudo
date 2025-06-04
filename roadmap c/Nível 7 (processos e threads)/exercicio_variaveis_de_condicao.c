00375823852571d5f85cae1cab91a0ae8632a220
Ferreira-Jefferson <jefferson.ctc.ferreira@outlook.com>
2025-06-04 15:22:41 -0300

    exercicio_variaveis_de_condicao.c

---
+++ roadmap c/Nível 7 (processos e threads)/exercicio_variaveis_de_condicao.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+
+#define NUM_GARCONS 5
+
+pthread_mutex_t mutex_pedido = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond_pedido_pronto = PTHREAD_COND_INITIALIZER;
+
+int pedido_pronto = 0;
+
+
+int qtd = 5;
+void* chef(void* args)
+{
+	int i = 0;
+	while(1)
+	{
+		pthread_mutex_lock(&mutex_pedido);
+		printf("Chef: preprando pedido %d\n", i+1);
+		sleep(1);
+
+		pedido_pronto = 1;
+		printf("Chef: pedido %d pronto\n", i+1);
+
+		pthread_cond_broadcast(&cond_pedido_pronto);
+		pthread_mutex_unlock(&mutex_pedido);
+
+		sleep(1); 
+		i++;
+	}
+	return (NULL);
+}
+
+void* garcom(void* args)
+{
+	int id = *(int*) args;
+
+	while(1)
+	{
+		pthread_mutex_lock(&mutex_pedido);
+		while(!pedido_pronto)
+		{
+			pthread_cond_wait(&cond_pedido_pronto, &mutex_pedido);
+		}
+
+		printf("Garcom %d: entreguando pedido.\n", id);
+		pedido_pronto = 0;
+		pthread_mutex_unlock(&mutex_pedido);
+		sleep(3);
+		printf("Garcom %d: pedido entregue\n", id);
+	}
+	return (NULL);
+}
+
+int main()
+{
+	pthread_t th_chef;
+	pthread_t th_garcom[qtd];
+	int id[qtd];
+	int i;
+
+	pthread_create(&th_chef, NULL, chef, NULL);
+
+	i = 0;
+	while(i < qtd)
+	{
+		id[i] = i + 1;
+		pthread_create(&th_garcom[i], NULL, garcom, &id[i]);
+		i++;
+	}
+
+	pthread_join(th_chef, NULL);
+
+	i = 0;
+	while(i < qtd)
+		pthread_join(th_garcom[i++], NULL);
+
+	pthread_mutex_destroy(&mutex_pedido);
+	pthread_cond_destroy(&cond_pedido_pronto);
+
+	printf("Restaurante encerrando operações!\n");
+
+	return (0);
+}
